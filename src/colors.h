efec4582ce90600bf1cf661c4fd83ccef4627998
abhinab-pradhan <166309936+abhinab-pradhan@users.noreply.github.com>
2025-08-17 10:42:18 +0530

    Add files via upload

---
+++ src/colors.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <raylib.h>
+#include <vector>
+
+//extern const  darkGrey;
+extern const Color darkGrey;
+extern const Color green;
+extern const Color red;
+extern const Color yellow;
+extern const Color purple;
+extern const Color cyan;
+extern const Color blue;
+
+std::vector<Color> getCelllColors();
