3d6551291e1627d039ab23f8c0793d24da471794
abhinab-pradhan <166309936+abhinab-pradhan@users.noreply.github.com>
2025-08-17 13:25:35 +0530

    Update block.h

--- src/block.h
+++ src/block.h
@@ -7,10 +7,14 @@ class Block{
     public:
         Block();
         void Draw();
+        void Move(int rows,int columns);
+        std::vector<Position> GetCellPosition();
         int id;
         std::map<int,std::vector<Position>> cells;
     private:
         int cellSize;
         int rotationState;
         std::vector<Color> colors;
+        int rowOffset;
+        int columnOffset;
 };
