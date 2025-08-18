c9fa8d13dce96bde610439feb494dda0e1b2d897
abhinab-pradhan <166309936+abhinab-pradhan@users.noreply.github.com>
2025-08-18 19:58:49 +0530

    Update grid.h

--- src/grid.h
+++ src/grid.h
@@ -8,6 +8,8 @@ class Grid
         void initialize();
         void print();
         void draw();
+        bool isCellOutSide(int row,int column);
+        bool isCellEmpty(int row,int column);
         int grid[20][20];
     private:
         int numRows;
