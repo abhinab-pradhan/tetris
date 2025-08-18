41c57eac4496a1648c4856fccc90da9fda807a59
abhinab-pradhan <166309936+abhinab-pradhan@users.noreply.github.com>
2025-08-18 19:31:57 +0530

    Update game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -25,3 +25,62 @@ void Game::Draw(){
     grid.draw();
     currentBlock.Draw();
 }
+
+void Game::handleInput(){
+    int keyPressed=GetKeyPressed();
+    switch (keyPressed)
+    {
+    case KEY_LEFT:
+        moveBlockLeft();
+        break;
+    case KEY_RIGHT:
+        moveBlockRight();
+        break;
+    case KEY_DOWN:
+        moveBlockDown();
+        break;
+    case KEY_UP:
+        rotateBlock();
+        break;
+    default:
+        break;
+    }
+}
+
+void Game::moveBlockLeft(){
+    currentBlock.Move(0,-1);
+    if(isBlockOutside()){
+        currentBlock.Move(0,1);
+    }
+}
+
+void Game::moveBlockRight(){
+    currentBlock.Move(0,1);
+    if(isBlockOutside()){
+        currentBlock.Move(0,-1);
+    }
+}
+
+void Game::moveBlockDown(){
+    currentBlock.Move(1,0);
+    if(isBlockOutside()){
+        currentBlock.Move(-1,0);
+    }
+}
+
+bool  Game::isBlockOutside(){
+    std::vector<Position> tiles=currentBlock.GetCellPosition();
+    for(Position item: tiles){
+        if(grid.isCellOutSide(item.row,item.column)){
+            return true;
+        }
+    }
+    return false;
+}
+
+void Game::rotateBlock(){
+    currentBlock.Rotate();
+    if(isBlockOutside()){
+        currentBlock.UndoRotation();
+    }
+}
